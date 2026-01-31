5c571c690d7abcd31400051470b95a7b5e3c6f41
Oliver <oliverkent048@gmail.com>
2026-01-31 10:28:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 function App() {
   return ( 
-    <div className="App">
+    <div className="App"> 
       <Navbar />
       <HeroSection />
       <ProductSection />
